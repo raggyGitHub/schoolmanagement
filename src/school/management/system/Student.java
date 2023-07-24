f3d9e6bca43452f07536065ee28ce42a6e795e94
ronald sarmiento <ronisarmont@gmail.com>
2023-07-24 11:46:35 -0400

    Student Class created

--- src/school/management/system/Student.java
+++ src/school/management/system/Student.java
@@ -1,5 +1,25 @@
 package school.management.system;
 
 public class Student {
-    
+    private int id;
+    private String name;
+    private int grade;
+    private int feesPaid;
+    private int feesTotal;
+
+    public Student(int id, String name, int grade, int feesPaid, int feesTotal) {
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+        this.feesPaid = 0;
+        this.feesTotal = 30000;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public void updateFeesPaid(int fees) {
+        feesPaid += fees;
+    }
 }
